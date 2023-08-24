b25edc32dea81bb1e80162ac7b2c362688a76e99
Keisuke Tarusawa <n11373466@qut.edu.au>
2023-08-25 04:28:07 +1000

    test

---
+++ Game/Connect4Game.cs
@@ -0,0 +1,188 @@
+﻿using System.Data.Common;
+
+namespace Game
+{
+    internal class Connect4Game : Game
+    {
+        const int ROW = 6, COLUMN = 7;
+
+
+        string[] symbol = new string[] { "O", "X" };
+
+        public Connect4Game(Board board, Player[] players, int curPlayer, HelpSystem helpSystem, HistoryManager historyManager, GameSaver gameSaver)
+            : base(board, players, curPlayer, helpSystem, historyManager, gameSaver)
+        {
+        }
+
+
+        public override bool CheckAndExeCmd(string cmd)
+        {
+            int column = int.Parse(cmd.Substring(5));
+            // put the piece on the board
+            for (int row = Board.Cells[Board.Cells.Length].Length - 1; row >= 0; row--)
+            {
+                if (board.Cells[row][column] == null)
+                {
+                    board.Cells[row][column] = new Piece(symbol[curPlayer]);
+                    
+                }
+            }
+            return false;
+        }
+        /*
+        public override bool ExecuteCmd(string cmd)
+        {
+            if (!CheckAndParseCmd(cmd)) return false;
+
+            if (cmd.StartsWith("drop "))
+            {
+                int column;
+                if (Int32.TryParse(cmd.Substring(5), out column))
+                {
+                    column--; // Adjust for 0-based indexing
+                    if (column >= 0 && column < board.Columns)
+                    {
+                        return DropPiece(column);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid column number.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid column number format.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unsupported command.");
+            }
+
+            return true;
+        }
+        */
+
+        /*
+
+        private bool DropPiece(int column)
+        {
+            for (int row = Board.Cells.Length - 1; row >= 0; row--)
+            {
+                if (board.Cells[row][column] == null)
+                {
+                    board.Cells[row][column] = new Piece(players[curPlayer].Symbol);
+                    return CheckWinner(row, column);
+                }
+            }
+
+            Console.WriteLine("Column is full. Try a different column.");
+            return true; // Continue with the same player's turn
+        }
+        */
+
+        public bool CheckChain()
+        {
+            // Check rows
+            for (int row = 0; row < 6; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    Piece piece1 = Board.Cells[row][col];
+                    Piece piece2 = Board.Cells[row][col + 1];
+                    Piece piece3 = Board.Cells[row][col + 2];
+                    Piece piece4 = Board.Cells[row][col + 3];
+
+                    if (piece1.Sign == symbol[curPlayer] && piece2.Sign == symbol[curPlayer] &&
+                        piece3.Sign == symbol[curPlayer] && piece4.Sign == symbol[curPlayer])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            // Check columns
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 7; col++)
+                {
+                    Piece piece1 = Board.Cells[row][col];
+                    Piece piece2 = Board.Cells[row + 1][col];
+                    Piece piece3 = Board.Cells[row + 2][col];
+                    Piece piece4 = Board.Cells[row + 3][col];
+                    if (piece1.Sign == symbol[curPlayer] && piece2.Sign == symbol[curPlayer] &&
+                        piece3.Sign == symbol[curPlayer] && piece4.Sign == symbol[curPlayer])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            // Check diagonals
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    Piece piece1 = Board.Cells[row][col];
+                    Piece piece2 = Board.Cells[row + 1][col + 1];
+                    Piece piece3 = Board.Cells[row + 2][col + 2];
+                    Piece piece4 = Board.Cells[row + 3][col + 3];
+                    Piece piece5 = Board.Cells[row][col + 3];
+                    Piece piece6 = Board.Cells[row + 1][col + 2];
+                    Piece piece7 = Board.Cells[row + 2][col + 1];
+                    Piece piece8 = Board.Cells[row + 3][col];
+                    if (piece1.Sign == symbol[curPlayer] && piece2.Sign == symbol[curPlayer] &&
+                        piece3.Sign == symbol[curPlayer] && piece4.Sign == symbol[curPlayer])
+                    {
+                        return true;
+                    }
+                    if (piece5.Sign == symbol[curPlayer] && piece6.Sign == symbol[curPlayer] &&
+                        piece7.Sign == symbol[curPlayer] && piece8.Sign == symbol[curPlayer])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public override List<string> CalculateLegalMoves()
+        {
+            List<string> legalMoves = new List<string>();
+            for (int column = 0; column < Board.Cells[Board.Cells.Length].Length; ++column)
+            {
+                legalMoves.Add($"drop {column + 1}");
+            }
+            return legalMoves;
+        }
+
+        protected override bool IsGameEnd()
+        {
+            if(CheckChain())
+                return true;
+
+            for (int i = 0; i < Board.Cells.Length; ++i)
+            {
+                for (int j = 0; j < Board.Cells[i].Length; ++j)
+                {
+                    if (Board.Cells[i][j] == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        protected override void CheckWinner()
+        {
+            if (CheckChain())
+                Console.WriteLine("{0} wins!", curPlayer);
+            else
+                Console.WriteLine("It's a draw.");
+        }
+    }
+}
+
+
---
+++ GameFactory/Connect4Factory.cs
@@ -0,0 +1,86 @@
+﻿using Sharprompt;
+
+
+namespace Game
+{
+    class Connect4Factory : GameFactory
+    {
+        public override Game CreateGame()
+        {
+            Player[] ps = InitPlayers();
+            Board b = InitBoard();
+            GameSaver gs = InitGameSaver();
+            HistoryManager hm = InitHistoryManager();
+            HelpSystem hs = InitHelpSystem();            
+            Game g = new Connect4Game(b, ps, 0, hs, hm, gs);
+            return g;
+        }
+
+        public override Game LoadGame()
+        {
+            Connect4Game g = null;
+            GameSaver gs = InitGameSaver();
+            HistoryManager hm = InitHistoryManager();
+            HelpSystem hs = InitHelpSystem();
+            string path = Prompt.Input<string>("Please enter the path of game saving file");
+            string json = IOUtil.ReadFromFile(path);
+            g = SerUtil.ToObj<Connect4Game>(json);
+            return new Connect4Game(g.Board, g.Players, g.CurPlayer, hs, hm, gs);
+        }
+
+        protected override Board InitBoard()
+        {
+            return new RegularBoard(6, 7);
+        }
+
+        protected override GameSaver InitGameSaver()
+        {
+            return new GameSaver();
+        }
+
+        protected override HelpSystem InitHelpSystem()
+        {
+            string name = "Connect4";
+            string description = "Two players take turns dropping pieces on a 7x6 board. The player forms an unbroken chain of four pieces horizontally, vertically, or diagonally, wins the game.";
+            List<string> helps = new List<string>()
+            {
+                "drop: drop <column>\n\t\te.g., 'drop 3' means drop a piece on 3rd column of the board",
+            };
+            return new HelpSystem(name, description, helps);
+        }
+
+        protected override HistoryManager InitHistoryManager()
+        {
+            return new HistoryManager();
+        }
+
+        protected override Player[] InitPlayers()
+        {
+            Player[] players = new Player[2];
+            List<string> options = new List<string>()
+            {
+                "Human vs Human",
+                "Human vs Computer (Human First)",
+                "Computer vs Human (Computer First)",
+            };
+            string choice = Prompt.Select("Choose the mode you want to play", options, defaultValue: options[0]);
+            int index = options.IndexOf(choice);
+            switch (index)
+            {
+                case 0:
+                    players[0] = new HumanPlayer("Player 1");
+                    players[1] = new HumanPlayer("Player 2");
+                    break;
+                case 1:
+                    players[0] = new HumanPlayer("Player 1");
+                    players[1] = new ComputerPlayer("Player 2");
+                    break;
+                case 2:
+                    players[0] = new ComputerPlayer("Player 1");
+                    players[1] = new HumanPlayer("Player 2");
+                    break;
+            }
+            return players;
+        }
+    }
+}
